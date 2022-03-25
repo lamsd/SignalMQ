1de5c3cd85487a1a145f1ea64ad84ee0882d2b48
lamsd <huynhlam14091994@gmail.com>
2022-03-26 00:45:24 +0700

    uploda data

--- rabbitMQ/README.md
+++ rabbitMQ/README.md
@@ -2,5 +2,15 @@
 ## introduction 
 RabbitMQ is aa messages broker. it accepts and forwards messages. You can think about it as a post office. when you put the mail  that you want posting in a post box, you can be sure that the letter carrier will evetually deliver the mail to your recipient, RabbitMQ is a post box, a post office, and a letter carrier.
 
+Install rabbit MQTT:
+
+```bash
+sudo apt-get install rabbitmq-server
+sudo systemctl start rabbitmq-server
+sudo systemctl enable rabbitmq-server
+```
+
+
+
 ## Refference:
 1. https://www.rabbitmq.com/tutorials/tutorial-one-python.html
---
+++ rabbitMQ/receive.py
@@ -0,0 +1,24 @@
+#!/home/lamsd/AI/AIpython3/my-app/bin python
+import queue
+import pika, sys, os
+
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host="192.168.31.240"))
+    channel = connection.channel()
+    channel.queue_declare(queue='hello')
+    def callback(ch, method, properties, body):
+        print(" [x] Received {}".format(body))
+    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack = True)
+    print(" [] waiting for messages. To exit press Ctrl+C")
+    channel.start_consuming()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Interrupted!")
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
+
